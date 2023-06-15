0084dadb02f812785a15370b1688b0a4aeb2f16d
shay213 <dawidpawelec@o2.pl>
2023-06-15 18:25:24 +0200

    Create home page

---
+++ src/components/buttons/SeeOurWorks.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const SeeOurWorks = ({ className }: { className: string }) => {
+  return <button className={className}>See Our Works</button>;
+};
+
+export default SeeOurWorks;
